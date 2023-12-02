ac3ce2af9b56344eb649b5b9a00110dc4c5ea131
Matthew Russo <matthew@matthewclayrusso.com>
2023-12-01 17:37:14 -0800

    Day1 : add solution for part 1

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,1 +1,15 @@
-let run = List.iter print_endline (Utils.read_lines "data/day1/part1.txt")
+let first_number line = Utils.char_to_int (List.find Utils.is_digit (Utils.to_chars line))
+
+let last_number line = Utils.char_to_int (List.find Utils.is_digit (List.rev (Utils.to_chars line)))
+
+let first_and_last_number line = ((first_number line), (last_number line))
+
+let day_1_data_lines = Utils.read_lines "data/day1/part1.txt"
+
+let first_and_last_number_of_each_line = List.map first_and_last_number day_1_data_lines
+
+let combined_first_and_last_numbers_of_each_line = List.map (fun (a, b) -> a * 10 + b) first_and_last_number_of_each_line
+
+let solution = Utils.sum combined_first_and_last_numbers_of_each_line
+
+let run = print_endline (string_of_int solution)
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,8 +1,23 @@
-(* read the entire file *)
+(* given a filename, read the entire file to a string *)
 let read_file file =
   In_channel.with_open_bin file In_channel.input_all
 
-(* read lines *)
+(* given a filename, read it to a list of strings, one for each line *)
 let read_lines file =
   let contents = In_channel.with_open_bin file In_channel.input_all in
-  String.split_on_char '\n' contents
+  String.split_on_char '\n' (String.trim contents)
+
+(* given a string, return a list of its characters *)
+let to_chars input = input |> String.to_seq |> List.of_seq
+
+(* a function that will validate if the input is a digit *)
+let is_digit digit =
+    match digit with
+     '0' .. '9' -> true
+    | _ -> false
+
+(* a function that will add the elements of a list together *)
+let sum xs = List.fold_left (fun x y -> x + y) 0 xs
+
+(* convert a char to its integer repr, e.g. '1' -> 1, '7' -> 7 *)
+let char_to_int c = int_of_string (String.make 1 c)
